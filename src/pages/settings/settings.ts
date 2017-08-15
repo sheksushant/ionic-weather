c3d0466fc3b7b32d1f743595cc752d5a7e97ca24
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-08-15 20:08:38 +0530

    added Settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../home/home';
 
-/**
- * Generated class for the SettingsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
+  city:string;
+  state:string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private storage:Storage) {
+
+      this.storage.get('location').then((val) => {
+        if(val != null){
+          let location = JSON.parse(val);
+          this.city = location.city;
+          this.state = location.state;
+        } else {
+          this.city = 'Miami';
+          this.state = 'FL';
+        }
+      });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+  }
+
+  saveForm(){
+    let location = {
+      city: this.city,
+      state: this.state
+    }
+    this.storage.set('location', JSON.stringify(location));
+    this.navCtrl.push(HomePage);
   }
 
 }
