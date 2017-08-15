f9acf7e29856b913b1e6d5ab8819e5746a6af3f0
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-08-15 19:18:19 +0530

    added weather API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather: any;
+  location: {
+    city : string,
+    state : string
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private weatherProvider:WeatherProvider) {
 
+  }
+  ionViewWillEnter() {
+    this.location = {
+      city: '12.8169358',
+      state: '80.04039829999999'
+    }
+    this.weatherProvider.getWeather(this.location.city,this.location.state).subscribe(weather => {
+      console.log(weather);
+    });
   }
 
 }
